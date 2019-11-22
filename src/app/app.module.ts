1c8147f0d97830ce1369fc456b250599cadc4604
Anthony <info@anthonyscheeres.nl>
2019-11-22 16:18:40 +0100

    working on login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
-
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AdminComponent
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          component:HomeComponent
+        },
+        {
+          path: "login",
+          component: LoginComponent
+        },
+        {
+          path: "admin",
+          component: AdminComponent
+        }
+      ]
+    )
   ],
-  providers: [],
+  providers: [
+     { provide: APP_BASE_HREF, useValue : '/' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
