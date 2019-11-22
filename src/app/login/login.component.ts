b9edfc04d4c1033e9605619ee805461e33e1b48a
Anthony <info@anthonyscheeres.nl>
2019-11-22 20:14:32 +0100

    login, register werkend met api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,7 +12,37 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
   loginUser(event){
-  event.preventDefault()
-    console.log(event)
+    const target = event.target
+
+  const username = target.querySelector('#username').value
+  const password = target.querySelector('#password').value
+
+      var xhr = new XMLHttpRequest();
+      var host = "localhost"
+      var port = "8080"
+  var url = "http://" + host + ":" + port +"/user/login";
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function () {
+
+  var data = JSON.stringify({
+  "username" : username,
+  "password" : password,
+  "id" : null,
+  "permission" : null,
+  "email" : null
+  });
+  xhr.send(data);
+
+
+  response = xhr.responseText
+  console.log(response)
+    if (!response.equals("fail")){
+      token = response
+      console.log(token)
+        //verander van pagina
+    }
+
+    }
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,6 +13,37 @@ export class RegisterComponent implements OnInit {
   }
   registerUser(event){
     event.preventDefault()
-      console.log(event)
-  }
+    const target = event.target
+
+const username= target.querySelector('#username').value
+const password = target.querySelector('#password').value
+const email = target.querySelector('#email').value
+
+      var xhr = new XMLHttpRequest();
+      var host = "localhost"
+      var port = "8080"
+var url = "http://" + host + ":" + port +"/user/register";
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.onreadystatechange = function () {
+
+  var data = JSON.stringify({
+  "username" : username,
+  "password" : password,
+  "id" : null,
+  "permission" : null,
+  "email" : email
+});
+  xhr.send(data);
+
+
+response = xhr.responseText
+console.log(response)
+    if (!response.equals("fail")){
+      //verander van pagina
+
+    }
+    }
+
+}
 }
