a1304fb43d077103e2e06bc6e6067c8f7f016f1c
Anthony <info@anthonyscheeres.nl>
2020-01-07 11:20:19 +0100

    .

---
+++ src/app/horizontale-navigation-bar/horizontale-navigation-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontale-navigation-bar',
+  templateUrl: './horizontale-navigation-bar.component.html',
+  styleUrls: ['./horizontale-navigation-bar.component.css']
+})
+export class HorizontaleNavigationBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/vertical-navigation-bar/vertical-navigation-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-navigation-bar',
+  templateUrl: './vertical-navigation-bar.component.html',
+  styleUrls: ['./vertical-navigation-bar.component.css']
+})
+export class VerticalNavigationBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
