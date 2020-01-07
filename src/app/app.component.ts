a0ab3b386894cbccb4ce7babe7429db33eef76e4
Anthony <info@anthonyscheeres.nl>
2020-01-07 19:31:13 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import DataModel from './models/DataModel';
+import { Router } from '@angular/router';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TestlabWeb';
+  show: boolean = DataModel.hiddenHamburger.show;
+  mySubscription: Subscription
+  constructor(private _router: Router) {
+    var time = 500
+    this.mySubscription = interval(time).subscribe((x => {
+      this.doStuff();
+    }));
+  }
+
+  ngOnInit() {
+  }
+  doStuff() {
+    this.show = DataModel.hiddenHamburger.show
+  }
+  checkCurrentPermission() {
+    this.show = !DataModel.account.hasSuperPermission;
+    //   console.log(DataModel.account.hasSuperPermission)
+  }
 }
+
+
+
