b43ecb0faac7da101db8a12752e4d44c6912d9cb
Anthony <info@anthonyscheeres.nl>
2020-01-16 10:57:15 +0100

    last merge before testing

--- src/app/models/AccountModel.ts
+++ src/app/models/AccountModel.ts
@@ -8,7 +8,7 @@ import { getHasWriteFromCookie, getHasDeleteFromCookie, getHasReadFromCookie, ge
 export class AccountModel {
 
 
-  token: String = localStorage.getItem("token");
+  token: String = null;
   hasWrite = false
   hasDelete = false
   hasRead = false
