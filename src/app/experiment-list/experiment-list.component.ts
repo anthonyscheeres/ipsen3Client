32f4f8c6501b51525bfe42677dbce9b841ace832
Overste <Thomas_warbout@hotmail.com>
2020-01-29 16:19:20 +0100

    WIP - added search service

--- src/app/experiment-list/experiment-list.component.ts
+++ src/app/experiment-list/experiment-list.component.ts
@@ -1,14 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ExperimentModel } from "../models/ExperimentModel";
-import { deleteExperiment, getExperiments } from "../services/experiment";
+import { deleteExperiment } from "../services/experiment";
 import {PopupService} from "../popup.service";
 import { ExistingExperimentComponent } from './existing-experiment/existing-experiment.component';
 import { CreateExperimentComponent } from '../create-experiment/create-experiment.component';
 import { getExperimentUrl } from './ExperimentUrl';
 import {FilterService} from "../filter.service";
 import {DecimalPipe} from "@angular/common";
-import {BehaviorSubject, Subject} from "rxjs";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-experiment-list',
@@ -23,25 +23,23 @@ import {BehaviorSubject, Subject} from "rxjs";
 
 export class ExperimentListComponent implements OnInit {
   dataFromServer: any;
-  sWsAvailable: BehaviorSubject<any> = new BehaviorSubject(this.dataFromServer);
+  dataFromServerAvailable: BehaviorSubject<any> = new BehaviorSubject(this.dataFromServer);
   modalService: any;
 
   constructor(private http: HttpClient, private popupService: PopupService, private filterService: FilterService) {
-    this.sWsAvailable.subscribe(
+    this.dataFromServerAvailable.subscribe(
       data => {
-        this.filterService.dataAvailable.next(data)
+        this.filterService.isDataAvailable.next(data)
       }
     )
   }
 
   showExperiments() {
-    console.log('start showexperiments')
     this.http.get<ExperimentModel[]>(
       getExperimentUrl())
       .subscribe(
         responseData => {
-          this.dataFromServer = responseData;
-          this.sWsAvailable.next(responseData);
+          this.dataFromServerAvailable.next(responseData);
         }
       )
   }
--- src/app/filter.service.ts
+++ src/app/filter.service.ts
@@ -1,55 +1,48 @@
-import {DecimalPipe} from "@angular/common";
 import {FormControl} from "@angular/forms";
 import {BehaviorSubject, Observable} from "rxjs";
 import {map, startWith} from "rxjs/operators";
-import {Injectable, PipeTransform} from "@angular/core";
+import {Injectable} from "@angular/core";
 
 @Injectable()
 export class FilterService {
-  data: any;
-  dataAvailable: BehaviorSubject<any> = new BehaviorSubject(this.data);
+  private _data: any;
+  private _isDataAvailable: BehaviorSubject<any> = new BehaviorSubject(this._data);
 
   observable$: Observable<any[]>;
   filter = new FormControl('');
 
-  constructor(pipe: DecimalPipe) {
-    this.dataAvailable.subscribe(
+  constructor() {
+    this._isDataAvailable.subscribe(
       data => {
-        this.data = this.dataAvailable.value;
-        this.observable$ = this.filter.valueChanges.pipe(
-          startWith(''),
-          map(text => this.search(text, pipe)),
-        );
-
-        // console.log('data ' + data)
-        // this.filterService.data = data;
+        if (typeof data !== "undefined") {
+          this._data = this._isDataAvailable.value;
+          this.observable$ = this.filter.valueChanges.pipe(
+            startWith(''),
+            map(text => this.search(text)),
+          );
+        }
       }
     )
-
-    // console.log(this.observable$)
-    // this.filter.valueChanges.pipe(
-    //   map(text => this.print())
-    // )
   }
 
-  print() {
-    console.log('test');
-  }
-
-  search(text: string, pipe: PipeTransform): any[] {
-    // if (this.data == 'undefined') {
-    //   return null;
-    // }
-
-    return this.data.filter(data => {
-
+  search(text: string): any[] {
+    return this._data.filter(data => {
       const term = text.toLowerCase();
 
-
-      return data.experiment_name.toLowerCase().includes(term)
-
+      for (let value of Object.values(data)) {
+        if (value.toString().toLowerCase().includes(term)) {
+          return value;
+        }
+      }
     });
   }
 
 
+  get isDataAvailable(): BehaviorSubject<any> {
+    return this._isDataAvailable;
+  }
+
+  set isDataAvailable(value: BehaviorSubject<any>) {
+    this._isDataAvailable = value;
+  }
 }
