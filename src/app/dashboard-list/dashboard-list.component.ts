98d0fdfa8e49809d52d8ef181f9a23f6c6621e6b
Overste <Thomas_warbout@hotmail.com>
2020-01-29 22:25:46 +0100

    Merged master-fix & merge-thomas together

--- src/app/dashboard-list/dashboard-list.component.ts
+++ src/app/dashboard-list/dashboard-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit} from '@angular/core';
-import DataModel from '../models/DataModel';
 import {Router} from '@angular/router';
 import {PopupService} from '../popup.service';
 
